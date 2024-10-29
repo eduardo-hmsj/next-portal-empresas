1efedc0b7ca7e75ac0a9e664c28290e40fbff012
Renan Lima <renanlima@MacBook-Air-de-Renan.local>
2024-10-29 15:36:26 -0300

    alterado imagem de logo

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+      {
+        name: 'next-portal-empresas', // Your project name
+        script: 'yarn', // For this example we're using npm, could also be yarn
+        args: 'start -p 3010', // Script to start the Strapi server, `start` by default
+      }
+    ]
+  }
+  
+  
